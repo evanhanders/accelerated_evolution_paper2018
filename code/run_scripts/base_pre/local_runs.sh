2dc8686461686f372d75c7fc831333c03c02ed87
Evan Anders <evan.anders@colorado.edu>
2017-12-13 16:13:57 -0700

    adds local run scripts and updates paper

---
+++ code/run_scripts/base_pre/local_runs.sh
@@ -0,0 +1,5 @@
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/base_pre --run_time_therm=1 --mixed_flux_T --no_slip --Rayleigh=2.79e3
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/base_pre --run_time_therm=1 --mixed_flux_T --no_slip --Rayleigh=6.01e3
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/base_pre --run_time_therm=1 --mixed_flux_T --no_slip --Rayleigh=1.30e4
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/base_pre --run_time_therm=1 --mixed_flux_T --no_slip --Rayleigh=2.79e4
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/base_pre --run_time_therm=1 --mixed_flux_T --no_slip --Rayleigh=6.01e4 --seed=150
---
+++ code/run_scripts/bvp_post/local_runs.sh
@@ -0,0 +1,6 @@
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/bvp_post --run_time_buoy=100 --mixed_flux_T --no_slip --Rayleigh=2.79e3 --restart=runs/bvp_pre/rayleigh_benard_2D_mixed_noSlip_Ra2.79e3_Pr1_a4.924/final_checkpoint/final_checkpoint_s1.h5
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/bvp_post --run_time_buoy=100 --mixed_flux_T --no_slip --Rayleigh=6.01e3 --restart=runs/bvp_pre/rayleigh_benard_2D_mixed_noSlip_Ra6.01e3_Pr1_a4.924/final_checkpoint/final_checkpoint_s1.h5
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/bvp_post --run_time_buoy=100 --mixed_flux_T --no_slip --Rayleigh=1.30e4 --restart=runs/bvp_pre/rayleigh_benard_2D_mixed_noSlip_Ra1.30e4_Pr1_a4.924/final_checkpoint/final_checkpoint_s1.h5
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/bvp_post --run_time_buoy=100 --mixed_flux_T --no_slip --Rayleigh=2.79e4 --restart=runs/bvp_pre/rayleigh_benard_2D_mixed_noSlip_Ra2.79e4_Pr1_a4.924/final_checkpoint/final_checkpoint_s1.h5
+mpirun -n 2 python3 rayleigh_benard.py --aspect=4.924 --nx=128 --nz=32 --root_dir=runs/bvp_post --run_time_buoy=100 --mixed_flux_T --no_slip --Rayleigh=6.01e4 --restart=runs/bvp_pre/rayleigh_benard_2D_mixed_noSlip_Ra6.01e4_Pr1_a4.924/final_checkpoint/final_checkpoint_s1.h5
+
