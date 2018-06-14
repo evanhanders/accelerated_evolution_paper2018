e04d7c3fc4bdbf35a812f4741a7f0664fa78ddf1
Evan H. Anders <evanhanders@gmail.com>
2018-06-13 20:31:07 -0600

    some updates to paper draft

--- code/plotting/nu_comp.py
+++ code/plotting/nu_comp.py
@@ -45,6 +45,12 @@ ax1 = axs[0]
 ax2 = axs[1]
 ax3 = axs[2]
 
+ax1_2 = ax1.twinx()
+ax2_2 = ax2.twinx()
+ax3_2 = ax3.twinx()
+
+
+
 
 scalars = h5py.File('{:s}/scalar_plots/scalar_values.h5'.format(full_dir), 'r')
 scalars2 = h5py.File('{:s}/scalar_plots/scalar_values.h5'.format(full_dir2), 'r')
@@ -68,27 +74,36 @@ T3  = scalars3['IE'].value
 T_final = np.mean(T2[-1000:])
 Nu_final = np.mean(nu2[-1000:])
 
+
+#Add horizontal final values
+ax1_2.axhline(T_final + 0.5,  c='blue')
+ax2_2.axhline(T_final + 0.5,  c='blue')
+ax1.axhline(Nu_final, c='dimgrey')
+ax2.axhline(Nu_final, c='dimgrey')
+
+
+
 time1 = np.concatenate((time, time3))
 nu1 = np.concatenate((nu, nu3))
-run_df = pd.DataFrame({'t':    time1[np.argmax(nu1)+50:],
-                        'nu':   nu1[np.argmax(nu1)+50:]})
+run_df = pd.DataFrame({'t':    time1[np.argmax(nu1)+5:],
+                        'nu':   nu1[np.argmax(nu1)+5:]})
 print(run_df)
-avg_nu1 = run_df.rolling(window=500, min_periods=50).mean()
+avg_nu1 = run_df.rolling(window=500, min_periods=50, axis=0, center=True).mean()
 avg_nu1 = np.array(avg_nu1)[:,1]
-ax1.plot(time1[nu1 > 0], nu1[nu1 > 0],   c='k')
-ax1.plot(run_df['t'], avg_nu1,   c='k', ls='--', lw=3, dashes=(5,1))
+ax1.plot(time1[nu1 > 0], nu1[nu1 > 0],   c='k', lw=2)
+ax1.plot(run_df['t'], avg_nu1,   c='dimgrey', lw=2)#, dashes=(5,1))
 #ax1.plot(time3, nu3, c='k')
 
-run2_df = pd.DataFrame({'t':    time2[np.argmax(nu2)+5:],
-                        'nu':   nu2[np.argmax(nu2)+5:]})
-avg_nu2 = run2_df.rolling(window=50, min_periods=5, axis=0).mean()
+run2_df = pd.DataFrame({'t':    time2[np.argmax(nu2)+2:],
+                        'nu':   nu2[np.argmax(nu2)+2:]})
+avg_nu2 = run2_df.rolling(window=50, min_periods=5, axis=0, center=True).mean()
 avg_nu2 = np.array(avg_nu2)[:,1]
 
-ax2.plot(time2[nu2 > 0], nu2[nu2 > 0], c='k')
-ax2.plot(run2_df['t'], avg_nu2,   c='k', lw=3, dashes=(5,1))
+ax2.plot(time2[nu2 > 0], nu2[nu2 > 0], c='k', lw=2)
+ax2.plot(run2_df['t'], avg_nu2,   c='dimgrey', lw=2)#, dashes=(5,1))
 
-ax3.plot(time2[nu2 > 0], nu2[nu2 > 0], c='k')
-ax3.plot(run2_df['t'], avg_nu2,   c='k', lw=3, dashes=(5,1))
+ax3.plot(time2[nu2 > 0], nu2[nu2 > 0], c='k', lw=2)
+ax3.plot(run2_df['t'], avg_nu2,   c='dimgrey', lw=2)#, dashes=(5,1))
 
 #ax1.set_xscale('log')
 ax1.set_yscale('log')
@@ -108,8 +123,9 @@ ax3.set_ylim(9e-1, 1e3)
 ax3.set_xticks([11200, 11400])
 ax3.set_xticklabels(("11200", "11400"))
 
-ax3.annotate("(a) SE", xy=(11330, 5e2))
-ax1.annotate("(b) AE", xy=(925, 5e2))
+#TODO: make bigger, drift lower and left.
+ax3.annotate("(a) SE", xy=(11280, 3e2), fontsize=16)
+ax1.annotate("(b) AE", xy=(875, 3e2), fontsize=16)
 
 ax1.set_xlabel("Time (freefall units)")
 #ax2.xaxis.set_label_coords(0.76, -0.2)
@@ -123,9 +139,6 @@ ax2.set_ylabel("Nu")
 #ax1.axvline(360.6, c='k', ls='--')
 
 
-ax1_2 = ax1.twinx()
-ax2_2 = ax2.twinx()
-ax3_2 = ax3.twinx()
 t1=76.8
 t2=278.3
 t3=360.6
@@ -187,12 +200,6 @@ ax2.plot((1,1+d/2, 1+d, 1 + 3*d/2), (1, 1+4*d, 1-4*d, 1), **kwargs)
 
 #Add in shaded box
 ax1.fill_between(time1[(time1>400)*(time1<600)], 5e0, 4e1, color='black', alpha=0.25)
-
-#Add horizontal final values
-ax1_2.axhline(T_final + 0.5, ls='--', c='blue')
-ax2_2.axhline(T_final + 0.5, ls='--', c='blue')
-ax1.axhline(Nu_final, ls='--', c='k')
-ax2.axhline(Nu_final, ls='--', c='k')
 
 #AE arrows
 ax1.arrow(120, 6.5e2, -19, 0, fc='k', ec='k', head_width=150, head_length=15)
